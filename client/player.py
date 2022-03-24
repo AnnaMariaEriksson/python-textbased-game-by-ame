18843bc9e6274820939360a6d41c107c67939816
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2022-03-24 11:32:24 +0100

    Created player class

---
+++ client/player.py
@@ -0,0 +1,14 @@
+from client.dreamhouse import Dreamhouse
+
+
+class Player:
+    def __init__(self, name, age, favorite_color, number_of_pets, has_pets = False):
+        self.name = name
+        self.age = age
+        self.hobbies = []
+        self.favorite_color = favorite_color
+        self.pet_counter = number_of_pets
+        self.has_pets = has_pets
+        self.pets = []
+
+
