e5ba7a45ce2793758fc446127d158fa3e9b25639
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2022-03-24 11:32:35 +0100

    created pet class

---
+++ client/pet.py
@@ -0,0 +1,7 @@
+class Pet:
+    def __init__(self, name, age, species, color, favorite_toy):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.color = color
+        self.favorite_toy = favorite_toy
