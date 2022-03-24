640ded122a0333b2eabeea24cd292cb3cbd07a83
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2022-03-24 11:33:22 +0100

    re-arranged some stuff in main file

--- client/main.py
+++ client/main.py
@@ -1,18 +1,26 @@
+from client.player import Player
+from methods import *
+
+
 def main():
     def hello():
         start_menu = '''
         Hi there!
         This is a textbased game in Python.
-        Or perhaps just a storyboard?
+        Or perhaps just a storytelling thing?
         I don't know yet.
         '''
-        return start_menu
-    def get_player_name():
-        player_name = input("What is your name?")
-        return f"Hello, {player_name}! Nice of you to join us."
-    
-    print(hello())
-    print(get_player_name())
+        print(start_menu)
+
+    def press_enter():
+        user_input = input("Press enter to continue...")
+        # print("\033")
+
+        return user_input
+
+    hello()
+    press_enter()
+    create_new_player()
 
 if __name__ == '__main__':
     main()
