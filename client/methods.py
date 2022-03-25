d206b3cc03d8f31880d387c2082fde205270d69c
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2022-03-25 14:23:26 +0100

    Fixed some bugs

--- client/methods.py
+++ client/methods.py
@@ -18,12 +18,11 @@ def create_new_player():
     if 'yes' in has_pets:
         has_pets = True
         number_of_pets = ask_question("How many pets do you have? ")
-        create_pets(int(number_of_pets))
         pets = create_pets(int(number_of_pets))
 
     else:
         has_pets = False
-    p = Player(name, age, favorite_color, number_of_pets, has_pets)
+    p = Player(name, age, favorite_color, int(number_of_pets), has_pets)
     p.pets.append(pets)
     return p
 
@@ -41,8 +40,7 @@ def ask_question(question, validator=None):
 
 def create_pets(number_of_pets):
     list_of_pets = []
-    i = 0
-    while i < number_of_pets:
+    for pet in range(number_of_pets):
         print("Ok, you have pets. Let's add them, shall we?")
         name = ask_question("What is your pet's name?")
         age = ask_question("How old is your pet? (write it with a number)")
@@ -52,7 +50,6 @@ def create_pets(number_of_pets):
 
         new_pet = Pet(name, age, species, color, favorite_toy)
         list_of_pets.append(new_pet)
-        i += 1
     return list_of_pets
 
 
@@ -60,21 +57,61 @@ def create_dreamhouse():
     print("Now I have a very personal question to ask. What is the house of your dreams?")
     size = ask_question("How large would it be? ")
     bedrooms = ask_question("How many bedrooms? ")
-    city_or_countryside = ask_question("Will it be located in a city or in the countryside? ")
-    if 'city' in city_or_countryside:
-        city = city_or_countryside
-    else:
-        city = None
-        countryside = True
+    city = ask_question("What city would it be located in. If countryside, what would the nearest town be? ")
     country = ask_question("What country will it be in? ")
-    has_yard = ask_question("Will it have a yard? ")
-    if 'yes' in has_yard:
-        has_yard = True
-        size_of_yard = ask_question("How big of a yard would you like to have? Answer in square meters")
-        floors = ask_question("How many floors? ")
-        price = ask_question("What would it cost to build or buy? Free is not an option here.")
-    else:
-        has_yard = False
+    size_of_yard = ask_question("How big of a yard would you like to have? (Answer in square meters) ")
+    floors = ask_question("How many floors? ")
+    price = ask_question("What would it cost to build or buy? Free is not an option here. "
+                         "(Answer in numbers without spaces) ")
 
-    dh = Dreamhouse(size, bedrooms, city, country, countryside, size_of_yard, floors, price, has_yard)
+    dh = Dreamhouse(size, bedrooms, city, country, size_of_yard, floors, price)
     return dh
+
+
+def presentation(player, dreamhouse, pet):
+    print(f'''
+    Ok, let's sum this information up.
+    Your name is {player.name} and you're {player.age} years old.
+    Your favorite color is {player.favorite_color}.
+    ''')
+    if player.has_pets:
+        if len(player.pets) > 1:
+            for pet in player.pets:
+                print(f"{pet.name} is {pet.age} years old, is a {pet.species}. {pet.name}'s favorite toy is {pet.favorite_toy}."
+                      f"{pet.name} is the color {pet.color}.")
+        else:
+            print(f"Your pet's name is {pet.name} and {pet.name} is {pet.age} years old. {pet.name} is the color {pet.color} and favorite toy is {pet.favorite_toy}")
+
+    print(f'''
+    Your dream house has {dreamhouse.bedrooms} bedrooms and {dreamhouse.floors} floors.
+    It's also at least {dreamhouse.size} square meters big.
+    The house is located in the city of {dreamhouse.city}.
+    The house of your dream has a yard that is at least {dreamhouse.size_of_yard} square meters big.
+    ''')
+
+
+def reading_letter(dreamhouse):
+    print(f'''
+    Ok, now the fun part starts.
+    Imagine that you've been away from your home for a while. Like at work or on a very weird vacation.
+    You've had a hard day.
+    You just want to go home. 
+    When you finally get there, you've gotten a letter in the mail.
+    You open it. 
+    The letter says:
+    
+    'Congrats! You've won {dreamhouse.price} SEK. You're free to do what you want with this cash, if you chose to spend it wisely.'
+    
+    What will you do? Spend it wisely, like for say - on your dream house? 
+    Or spend it on booze and gambling? Your choice.
+    
+    Type in A for spending it wisely, and B for the other choice.
+    ''')
+
+    user_input = input("A or B, what will you choose? ")
+
+    if 'A' in user_input:
+        print("Yay, you win. You get to buy or build your dream house. Good choice.")
+    else:
+        print("You lose. I'm so disappointed... sad face and all that crap.")
+        exit()
